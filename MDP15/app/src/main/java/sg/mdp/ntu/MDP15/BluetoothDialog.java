ba917308a67cdeaf7a3e6865e43a241ba86eb82a
How Yi De <etherhowyd@gmail.com>
2020-08-18 23:05:21 +0800

    Added Bluetooth ON OFF

--- MDP15/app/src/main/java/sg/mdp/ntu/MDP15/BluetoothDialog.java
+++ MDP15/app/src/main/java/sg/mdp/ntu/MDP15/BluetoothDialog.java
@@ -2,10 +2,17 @@ package sg.mdp.ntu.MDP15;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatDialogFragment;
@@ -14,6 +21,10 @@ public class BluetoothDialog extends AppCompatDialogFragment {
 
     private EditText editusername;
     private EditText editpassword;
+    private final String TAG = "BluetoothDialog";
+    private BluetoothAdapter mBluetoothAdapter;
+
+    Button btnONOFF;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
@@ -22,7 +33,64 @@ public class BluetoothDialog extends AppCompatDialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.layout_bluetooth,null);
 
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        btnONOFF = (Button)view.findViewById(R.id.btnONOFF);
+        btnONOFF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: enabling/disabling BT");
+                enableDisableBT();
+            }
+        });
+
         builder.setView(view);
         return builder.create();
     }
+
+    private void enableDisableBT() {
+        if(mBluetoothAdapter == null){
+            Log.d(TAG,"enableDisableBT: Does not have BT capabilities");
+        }
+        if(!mBluetoothAdapter.isEnabled()){
+            Log.d(TAG,"enableDisableBT: enabling BT");
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            requireActivity().registerReceiver(receiver,BTIntent);
+        }
+        if (mBluetoothAdapter.isEnabled()) {
+            Log.d(TAG,"enableDisableBT: disabling BT");
+            mBluetoothAdapter.disable();
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            requireActivity().registerReceiver(receiver,BTIntent);
+        }
+    }
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,mBluetoothAdapter.ERROR);
+
+                switch(state){
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG,"onReceive:STATE OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG,"onReceive:STATE TURNING OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG,"onReceive:STATE ON");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG,"onReceive:STATE TURNING ON");
+                        break;
+                }
+            }
+        }
+    };
 }
